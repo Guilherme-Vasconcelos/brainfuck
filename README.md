7455089420f9b72cb5310165109dd5ec7b8e69be
Guilherme Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2024-09-23 09:18:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# brainfuck
+A brainfuck compiler.
+
+## License
+GNU General Public License, either version 3 or any later versions at your choice. See [LICENSE.txt](./LICENSE.txt) for more information.
---
+++ brainfuck/__main__.py
@@ -0,0 +1,9 @@
+def main() -> None:
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    # Do not put any other code here, as poetry's `brainfuck` entrypoint
+    # executes the `main` function directly (i.e. anything out of `main` will
+    # not be executed if running `$ poetry run brainfuck`).
+    main()
